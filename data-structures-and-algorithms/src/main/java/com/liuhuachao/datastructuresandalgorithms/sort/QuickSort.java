1cc69e4f0011cb6d0c62275eb0229a8cf8c4e0a6
liuhuachao <lhc@bill-sj.com>
2021-12-08 18:44:27 +0800

    feat：update QuickSort

--- data-structures-and-algorithms/src/main/java/com/liuhuachao/datastructuresandalgorithms/sort/QuickSort.java
+++ data-structures-and-algorithms/src/main/java/com/liuhuachao/datastructuresandalgorithms/sort/QuickSort.java
@@ -13,6 +13,12 @@ public class QuickSort {
 
 	/**
 	 * 快速排序
+	 * 实现原理：选择一个关键值作为基准值，将比基准值大的都放在右边的序列中，将比基准值小的都放在左边的序列中。
+	 * 具体的过程如下：
+	 *（1）从后向前比较，用基准值和最后一个值进行比较。如果比基准值小，则交换位置；如果比基准值大，则继续比较下一个值，直到找到第1个比基准值小的值才交换位置；
+	 *（2）在从后向前找到第1个比基准值小的值并交换位置后，从前向后开始比较。如果有比基准值大的，则交换位置；如果没有，则继续比较下一个，直到找到第1个比基准值大的值才交换位置；
+	 *（3）重复执行以上过程，直到从前向后比较的索引大于等于从后向前比较的索引，则结束一次循环。这时对于基准值来说，左右两边都是有序的数据序列；
+	 *（4）重复循环以上过程，分别比较左右两边的序列，直到整个数据序列有序。
 	 * @param arr
 	 * @param left
 	 * @param right
@@ -25,17 +31,8 @@ public class QuickSort {
 		// 基准值，选择第1个元素为基准元素
 		int key = arr[left];
 
-		while (start < end) {
-
-			// 从前往后比较
-			while (start < end && arr[start] < key) {
-				start++;
-			}
-			if (arr[start] >= key) {
-				int temp = arr[start];
-				arr[start] = key;
-				key = temp;
-			}
+		// 将比基准值大的都放在右边的序列中，将比基准值小的都放在左边的序列中
+		while (end > start) {
 
 			// 从后向前比较
 			while (end > start && arr[end] > key) {
@@ -46,74 +43,28 @@ public class QuickSort {
 				arr[end] = key;
 				key = temp;
 			}
+
+			// 从前往后比较
+			while (start < end && arr[start] < key) {
+				start++;
+			}
+			if (arr[start] >= key) {
+				int temp = arr[start];
+				arr[start] = key;
+				key = temp;
+			}
 		}
 
-		// 上面排序完成后，左边的值都比关键值小，右边的都比关键值大，但左右两边内部还未排序。
-		// 递归调用快速排序，不包含关键值
+		// 左边序列：递归调用快速排序，不包含基准值
 		if (start > left) {
 			quickSort(arr, left, start - 1);
 		}
 
+		// 右边序列：递归调用快速排序，不包含基准值
 		if (end < right) {
 			quickSort(arr, end + 1, right);
 		}
 
 		return arr;
 	}
-
-	/**
-	 * 快速排序
-	 * @param arr
-	 * @param left
-	 * @param right
-	 * @return
-	 */
-	public static int[] quickSort2(int[] arr, int left, int right) {
-		if (right > left) {
-			// 原序列划分后基准元素的位置
-			int base_index = partition(arr, left, right);
-
-			// 对第一个子序列快速排序，不包含基准元素
-			quickSort2(arr, left, base_index - 1);
-
-			// 对第二个子序列快速排序，不包含基准元素
-			quickSort2(arr, base_index + 1, right);
-		}
-		return arr;
-	}
-
-	/**
-	 * 序列划分
-	 * @param arr
-	 * @param left
-	 * @param right
-	 * @return
-	 */
-	public static int partition(int[] arr, int left, int right) {
-		// 基准元素的值
-		int base = arr[left];
-		// 基准元素最终应该在的位置
-		int base_index = left;
-
-		// 从基准元素的下一个元素开始
-		for (int i = left + 1; i <= right; i++) {
-			if (arr[i] < base) {
-				// 若其小于基准元素,则基准元素最终位置后移；否则不用移动
-				base_index++;
-
-				if (base_index != i) {
-					int temp = arr[base_index];
-					arr[base_index] = arr[i];
-					arr[i] = temp;
-				}
-			}
-		}
-
-		// 将基准元素就位
-		arr[left] = arr[base_index];
-		arr[base_index] = base;
-
-		return base_index;
-	}
-
 }
