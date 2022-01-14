570abe4c4b7c3be30a12ecffc810e9ef11827f5e
liuhuachao <lhc@bill-sj.com>
2022-01-14 11:27:35 +0800

    style：整理格式

--- design-patterns/src/main/Singleton.java
+++ design-patterns/src/main/Singleton.java
@@ -37,7 +37,7 @@ public class Singleton{
 	}
 
 	/**
-	 * 双重校验锁
+	 * 双重检查
 	 * 双锁模式指在懒汉模式的基础上做进一步优化，给静态对象的定义加上volatile锁来保障初始化时对象的唯一性，在获取对象时通过synchronized(Singleton.class)给单例类加锁来保障操作的唯一性。
 	 */
 	protected static class Lock2Singleton{
@@ -53,8 +53,6 @@ public class Singleton{
 			}
 			return instance;
 		}
-
-
 	}
 
 	/**
